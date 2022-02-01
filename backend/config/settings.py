d1c7d1766234bd8f0aac2712fb3a9a8000bbed14
rajitechis <rajit@techis.io>
2022-02-01 15:18:17 +0530

    last

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = 'django-insecure-ov#r^^&xv&^0vmc(zj&h_t^$*52@8jicn=%*z*@s-=li!s_p@=
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://forum-prod-frontend.herokuapp.com']
+CORS_ALLOW_ALL_ORIGINS = True
+#CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://forum-prod-frontend.herokuapp.com']
 
 # Application definition
 
@@ -89,24 +89,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # Heroku Database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'dch7n72frg031v',
-#         'USER': 'ldpelxievviuhf',
-#         'PORT': 5432,
-#         'HOST': 'ec2-23-23-164-251.compute-1.amazonaws.com',
-#         'PASSWORD': '8a9353d3c2aa7a6baaf128ed3c6fc0a4ede1cccfec1ccc0222e089e0871b8975',
-#     }
-#}
-
-# Local Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd7geafjb85am38',
+        'USER': 'swfkqiwvacobry',
+        'PORT': 5432,
+        'HOST': 'ec2-3-219-204-29.compute-1.amazonaws.com',
+        'PASSWORD': '88012e56d02eb4a9af6e8d666de9f0ec67649297b66a6b2fac9debbda29b7b2f',
     }
 }
+
+# Local Database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Heroku PostgreSQL Database
 django_heroku.settings(locals())
--- frontend/src/API.js
+++ frontend/src/API.js
@@ -7,9 +7,10 @@ var baseURL;
 // ) {
 //   baseURL = process.env.REACT_APP_API_BASE_URL;
 // } else {
-baseURL = "http://127.0.0.1:8000";
+// baseURL = "http://127.0.0.1:8000";
 // }
-// baseURL="https://ethiopianvisitorsbackend.herokuapp.com/";
+
+baseURL = "https://european-visitor-backend.herokuapp.com/";
 const api = axios.create({
   baseURL: baseURL,
   headers: {
